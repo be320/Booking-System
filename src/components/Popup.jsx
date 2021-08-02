2048ffdd2b094d55ccbee4c54d416a4b026d7427
Ahmed Bahaa <ahmedbahaa.3@hotmail.com>
2021-08-02 22:41:11 +0200

    Added Date Picker Component

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Modal from "@material-ui/core/Modal";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+import "./Popup.css";
+import IconButton from "@material-ui/core/IconButton";
+import CancelIcon from "@material-ui/icons/Cancel";
+import Button from "@material-ui/core/Button";
+import DatePicker from './DatePicker'
 
 const useStyles = makeStyles(theme => ({
   paper: {
     position: "absolute",
-    width: 400,
+    width: "60vh",
+    height: "50vh",
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
+    padding: theme.spacing(2, 4, 3),
+    justifyContent: "center"
   }
 }));
 
@@ -22,12 +29,38 @@ const Popup = props => {
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
     >
-      <div className={classes.paper}>
-        <h2 id="simple-modal-title">Text in a modal</h2>
-        <p id="simple-modal-description">
-         {props.resource.name}
-        </p>
-        {/* <SimpleModal /> */}
+      <div style={{ top: "15vh", left: "70vh" }} className={classes.paper}>
+        <div className="popup-row">
+          <p>Booking {props.resource.name}</p>
+          <IconButton
+            color="primary"
+            aria-label="close"
+            onClick={props.handleClose}
+            style={{ marginLeft: "auto" }}
+          >
+            <CancelIcon />
+          </IconButton>
+        </div>
+        <hr />
+        <div className="popup-row">
+          <p>Date From</p>
+          <DatePicker />
+        </div>
+        <div className="popup-row">
+          <p>Date To</p>
+          <DatePicker />
+        </div>
+        <div className="popup-row">
+          <p>Quantity</p>
+        </div>
+        <div className="popup-row">
+          <Button
+            variant="contained"
+            color="primary"
+          >
+            Book
+          </Button>
+        </div>
       </div>
     </Modal>
   );
